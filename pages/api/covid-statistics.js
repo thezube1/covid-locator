dcbee2530ac8ef14d22f0f7bee71b6c9fc166881
Zubin <zubin.hydrie@bush.edu>
2021-08-01 12:01:24 -0700

    Test

--- pages/api/covid-statistics.js
+++ pages/api/covid-statistics.js
@@ -3,7 +3,7 @@ const fs = require("fs");
 const util = require("util");
 const path = require("path");
 
-import defaultFunc from "../../public/countries/default";
+import defaultFunc from "../../countries/default";
 
 export default async function handler(req, res) {
   if (req.method === "POST") {
