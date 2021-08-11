94236a1f1731b2938a44e73ac804d5da7eaa7ed2
Zubin <zubin.hydrie@bush.edu>
2021-08-10 22:42:42 -0700

    Added data sources

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Link from "next/link";
 import Navbar from "../components/navbar/navbar";
+import Locate from "../pages/locate";
 
 export default function Home() {
   return (
@@ -16,9 +17,41 @@ export default function Home() {
           <div className="title" id="home-title">
             <span id="home-covid">COVID</span> Locator
           </div>
+
           <Link href="/locate">
             <div className="button">Continue</div>
           </Link>
+          <div className="seperator"></div>
+          <div id="home-data-wrapper">
+            <div className="text">Our Data Sources:</div>
+            <a
+              rel="noopener noreferrer"
+              target="_blank"
+              href="https://github.com/CSSEGISandData/COVID-19"
+            >
+              <div className="text home-data-source" style={{ marginLeft: 0 }}>
+                CSSE @ John Hopkins COVID data
+              </div>
+            </a>
+            <a
+              rel="noopener noreferrer"
+              target="_blank"
+              href="https://dev.socrata.com/foundry/data.cdc.gov/8xkx-amqh"
+            >
+              <div className="text home-data-source" style={{ marginLeft: 0 }}>
+                CDC Vaccination API
+              </div>
+            </a>
+            <a
+              rel="noopener noreferrer"
+              target="_blank"
+              href="https://www.census.gov/data/developers/data-sets/acs-1year.html"
+            >
+              <div className="text home-data-source" style={{ marginLeft: 0 }}>
+                Census data API (2019)
+              </div>
+            </a>
+          </div>
         </div>
       </div>
     </>
